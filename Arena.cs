eb3ef37fb3b7c4b53356c2dd0efd24268252c104
Eduard <49207268+Eduard44@users.noreply.github.com>
2019-06-19 18:52:04 +0300

    FightingArena
    classes and object methods

---
+++ Arena.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FightingArena
+{
+    public class Arena
+    {
+        private Dictionary<string, Gladiator> gladiators;
+
+        public Arena(string name)
+        {
+            Name = name;
+
+            gladiators = new Dictionary<string, Gladiator>();
+        }
+
+        public string Name { get; set; }
+
+        public void Add(Gladiator gladiator)
+        {
+            if (!gladiators.ContainsKey(gladiator.Name))
+            {
+                bool isValid = true;
+                if (gladiators.Count > 0)
+                {
+                    foreach (var item in gladiators)
+                    {
+                        int gladiatorStatPow = gladiator.GetStatPower();
+                        int gladiatorWeaponPow = gladiator.GetWeaponPower();
+                        int gladiatorTotalPow = gladiator.GetTotalPower();
+
+                        int oponentStatPow = item.Value.GetStatPower();
+                        int oponentWeaponPow = item.Value.GetWeaponPower();
+                        int oponentTotalPow = item.Value.GetTotalPower();
+
+                        if (gladiatorStatPow == oponentStatPow &&
+                            gladiatorWeaponPow == oponentWeaponPow &&
+                            gladiatorTotalPow == oponentTotalPow)
+                        {
+                            isValid = false;
+                        }
+                    }
+                }
+                if (isValid)
+                {
+                    gladiators.Add(gladiator.Name, gladiator);
+                }
+            }
+        }
+
+        public void Remove(string gladiator)
+        {
+            if (gladiators.ContainsKey(gladiator))
+            {
+                gladiators.Remove(gladiator);
+            }
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            if (Count > 0)
+            {
+                int highScore = 0;
+                string champion = "";
+                foreach (var item in gladiators)
+                {
+                    if (item.Value.GetStatPower() >= highScore)
+                    {
+                        highScore = item.Value.GetStatPower();
+                        champion = item.Key;
+                    }
+                }
+                return gladiators[champion];
+            }
+            return null;
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            if (Count > 0)
+            {
+                int highScore = 0;
+                string champion = "";
+                foreach (var item in gladiators)
+                {
+                    if (item.Value.GetWeaponPower() >= highScore)
+                    {
+                        highScore = item.Value.GetWeaponPower();
+                        champion = item.Key;
+                    }
+                }
+                return gladiators[champion];
+            }
+            return null;
+        }
+
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            if (Count > 0)
+            {
+                int highScore = 0;
+                string champion = "";
+                foreach (var item in gladiators)
+                {
+                    if (item.Value.GetTotalPower() >= highScore)
+                    {
+                        highScore = item.Value.GetTotalPower();
+                        champion = item.Key;
+                    }
+                }
+                return gladiators[champion];
+            }
+            return null;
+        }
+
+        public int Count
+        {
+            get { return gladiators.Count; }
+        }
+
+        public override string ToString()
+        {
+            string result = $"{Name} - {Count}" +
+                $" gladiators are participating.";
+            return result;
+        }
+    }
+}
---
+++ Gladiator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FightingArena
+{
+    public class Gladiator
+    {
+        public Gladiator(string name, Stat stat, Weapon weapon)
+        {
+            Name = name;
+            Stat = stat;
+            Weapon = weapon;
+        }
+
+        public string Name { get; set; }
+
+        public Stat Stat { get; set; }
+
+        public Weapon Weapon { get; set; }
+
+        public int GetTotalPower()
+        {
+            int totalPower = GetStatPower() + GetWeaponPower();
+            return totalPower;
+        }
+
+        public int GetStatPower()
+        {
+            int statSum = Stat.Strength
+                + Stat.Flexibility
+                + Stat.Agility
+                + Stat.Skills
+                + Stat.Intelligence;
+            if (statSum<=0)
+            {
+                return 0;
+            }
+            return statSum;
+        }
+
+        public int GetWeaponPower()
+        {
+            int weaponSum = Weapon.Size
+                + Weapon.Solidity
+                + Weapon.Sharpness;
+            if (weaponSum <= 0)
+            {
+                return 0;
+            }
+            return weaponSum;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{Name} - {GetTotalPower()}" +
+                Environment.NewLine +
+                $"  Weapon Power: {GetWeaponPower()}" +
+                Environment.NewLine +
+                $"  Stat Power: {GetStatPower()}";
+            return result;
+        }
+    }
+}
